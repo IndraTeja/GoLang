674776a98c3216d7befe978ab863ac1c7a6866a8
Indra Teja Chintakayala <indrateja@gmail.com>
2019-01-11 11:40:30 -0600

    binary, buddle

--- A2/binary_sort.go
+++ A2/binary_sort.go
@@ -2,21 +2,36 @@ package main
 
 import "fmt"
 
-//func isBinary(arr int) int {
-//	for i:=0; i <= 8; i++ {
-//		if arr[i] ==
-//	}
-//}
+func isBinary(arr []int, n int, find int) int {
+	//mid := n/2
+	var position int
+	//first, last := 0,n
+	for i:=0; i<n; i++ {
+		if arr[i] == find {
+			position = i + 1
+			break
+		}
+	}
+	return position
+}
+
 
 func main()  {
-	var arr [20]int
+	// n reads and stores the length of array
 	var n int
-	//{1,3,10,23,45,67,90,100}
-	fmt.Println("Hello : ")
+	fmt.Println("Enter length of array : ")
 	fmt.Scanf("%d",&n)
-	for i:0; i<=n; i++ {
-
+	// arr of length n is created
+	arr := make([]int, n)
+	// ask user to enter a number to search in array
+	for i:=0; i<n; i++{
+		fmt.Println("Enter array numbers : ")
+		fmt.Scanf("%d",&arr[i])
 	}
-	fmt.Scanf("%d",&arr)
-	fmt.Println(arr)
+
+	var find int
+	fmt.Println("Enter the number you want to find : ")
+	fmt.Scanf("%d",&find)
+	fmt.Print("Found at position : ")
+	fmt.Println(isBinary(arr, n, find))
 }
