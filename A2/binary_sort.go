c255966657b8237c0138101263d986adf3612857
Indra Teja Chintakayala <indrateja@gmail.com>
2019-01-10 18:06:45 -0600

    Binary

--- A2/binary_sort.go
+++ A2/binary_sort.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+//func isBinary(arr int) int {
+//	for i:=0; i <= 8; i++ {
+//		if arr[i] ==
+//	}
+//}
+
+func main()  {
+	var arr [20]int
+	var n int
+	//{1,3,10,23,45,67,90,100}
+	fmt.Println("Hello : ")
+	fmt.Scanf("%d",&n)
+	for i:0; i<=n; i++ {
+
+	}
+	fmt.Scanf("%d",&arr)
+	fmt.Println(arr)
+}
