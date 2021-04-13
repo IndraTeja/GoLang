611ebc3678e5ff63359a798201b62c56fe2618d6
Indra Teja Chintakayala <indrateja@gmail.com>
2021-04-13 18:46:25 -0500

    index

--- todd-exercise/exercise2-1.go
+++ todd-exercise/exercise2-1.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func even(a int) (int, bool) {
+
+	if a%2 == 0 {
+		return a / 2, true
+	} else {
+		return a / 2, false
+	}
+
+}
+
+func main() {
+
+	var a int
+
+	fmt.Scan("%d", &a)
+
+	fmt.Println(even(a))
+
+}
